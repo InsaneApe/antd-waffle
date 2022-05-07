8fff4a24ef4e0b1c440206045c32f55e3da728ce
Hao.li <v-haoli@handshakes.com.sg>
2022-05-07 10:48:47 +0800

    remove import

--- src/components/login/Login.stories.tsx
+++ src/components/login/Login.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { LoginComponent } from './login.component';
-import { COMPONENTS } from 'src/constants/common';
 
 export default {
   title: 'UI components/Login',
