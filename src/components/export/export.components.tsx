3246fba24ac04c408e84666f55981dcd532a6197
Hao.li <v-haoli@handshakes.com.sg>
2022-05-10 10:44:26 +0800

    Add download xlsx

--- src/components/export/export.components.tsx
+++ src/components/export/export.components.tsx
@@ -4,25 +4,44 @@ import { CSVLink } from "react-csv";
 import { DownloadOutlined } from '@ant-design/icons';
 import { BaseButtonProps } from "antd/lib/button/button";
 import classnames from 'classnames';
+import { FileType } from "@constants/common";
 import './export.less';
 
 
 export interface IExportComponentProps extends BaseButtonProps {
   className?: string;
-  data?: string[][]
+  label: string;
+  data?: any[];
+  headers?: any[];
+  fileName?: string;
+  fileType?: string;
+  exportToXlsx?: () => void;
 }
 
 const ExportComponent = (props: IExportComponentProps) => {
-  const { className, data, ...other } = props;
+  const { className, data, label, headers, fileName, fileType, exportToXlsx, ...other } = props;
+
+
+  const downloadType = () => {
+    if (fileType === FileType.CSV) {
+      return (
+        <CSVLink data={data} headers={headers} filename={fileName} className="download-link">
+          {label}
+        </CSVLink>
+      );
+    }
+    return <>{label}</>;
+  };
 
   return (
     <React.Fragment>
       <Button
         className={classnames('download-btn',className)}
+        onClick={exportToXlsx}
         icon={<DownloadOutlined />}
         {...other}
       >
-        <CSVLink data={data} className="download-link">Download</CSVLink>
+        {downloadType()}
       </Button>
     </React.Fragment>
   );
--- src/components/export/export.stories.tsx
+++ src/components/export/export.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import ExportComponent from './export.components';
+import { FileType } from '@constants/common';
+import FileSaver from "file-saver";
+import * as XLSX from 'xlsx';
 
 export default {
   title: 'UI components/Export',
@@ -10,16 +13,70 @@ export default {
 
 const Template: ComponentStory<typeof ExportComponent> = (args) => <ExportComponent {...args} />;
 
-export const Default = Template.bind({});
 
-const csvData = [
-  ["firstname", "lastname", "email"],
-  ["Ahmed", "Tomi", "ah@smthing.co.com"],
-  ["Raed", "Labes", "rl@smthing.co.com"],
-  ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+const headers = [
+  { label: "First Name", key: "firstName" },
+  { label: "Last Name", key: "lastName" },
+  { label: "Email", key: "email" }
 ];
 
-Default.args = {
-  data: csvData,
-  type: 'primary'
+const csvData = [
+  { firstName: "Ahmed", lastName: "Tomi", email: "ah@smthing.co.com" },
+  { firstName: "Raed", lastName: "Labes", email: "rl@smthing.co.com" },
+  { firstName: "Yezzi", lastName: "Min l3b", email: "ymin@cocococo.com" }
+];
+
+const wscols = [
+  { wch: Math.max(...csvData.map(customer => customer.firstName.length)) },
+  { wch: Math.max(...csvData.map(customer => customer.lastName.length)) },
+  { wch: Math.max(...csvData.map(customer => customer.email.length)) }
+];
+const Heading = [
+  {
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "Email",
+  }
+];
+
+const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+
+const exportToXlsx = () => {
+  const ws = XLSX.utils.json_to_sheet(Heading, {
+    header: ["firstName", "lastName", "email"],
+    skipHeader: true,
+    origin: 0
+  } as any);
+  ws["!cols"] = wscols;
+  XLSX.utils.sheet_add_json(ws, csvData, {
+    header: ["firstName", "lastName", "email"],
+    skipHeader: true,
+    origin: -1
+  });
+  const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+  const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+  const data = new Blob([excelBuffer], { type: fileType });
+  FileSaver.saveAs(data, 'downloadFile.xlsx');
 };
+
+export const DownloadCSV = Template.bind({});
+export const DownloadXlsx = Template.bind({});
+
+DownloadCSV.args = {
+  data: csvData,
+  type: 'primary',
+  label: 'Download CSV',
+  headers: headers,
+  fileName: 'Download CSV',
+  fileType: FileType.CSV
+};
+
+DownloadXlsx.args = {
+  data: csvData,
+  type: 'primary',
+  label: 'Download XLSX',
+  headers: headers,
+  fileName: 'Download XLSX',
+  fileType: FileType.XLSX,
+  exportToXlsx: exportToXlsx
+}
