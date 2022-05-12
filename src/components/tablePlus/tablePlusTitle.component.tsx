e1b91f8ca9041e7ceff227e68e7d3d6d50c09392
littleforest <670177821@qq.com>
2022-05-12 15:45:34 +0800

    feat: 新增表格标题

---
+++ src/components/tablePlus/tablePlusTitle.component.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import { isString } from 'lodash';
+
+interface TablePlusTitleProps {
+  title?: string | ReactNode
+}
+
+const TablePlusTitle = (props:TablePlusTitleProps) => {
+
+  const { title } = props;
+
+  return (
+    <div className="AntdPrivate-title">
+      {
+        isString(title)?
+        <div className="AntdPrivate-title-content">
+          {title}
+        </div>:
+        <>{title}</>
+      }
+    </div>
+  )
+};
+
+export default TablePlusTitle;
