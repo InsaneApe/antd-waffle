b1c2fea6a0f8fa323df85ed339d96bb520a3b121
littleforest <670177821@qq.com>
2022-06-02 09:34:43 +0800

    fix: 修复全屏组件

--- src/components/fullScreen/fullScreen.example.tsx
+++ src/components/fullScreen/fullScreen.example.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FullScreen, { useFullScreenHandle } from './index';
+import {FullScreen, useFullScreenHandle } from './index';
 import {Button} from 'antd';
 
 const FullScreenExample = () => {
--- src/components/fullScreen/index.tsx
+++ src/components/fullScreen/index.tsx
@@ -3,7 +3,7 @@ import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 
 export type {FullScreenProps,FullScreenHandle} from 'react-full-screen';
 
-export {
+export default {
   useFullScreenHandle,
   FullScreen
 };
