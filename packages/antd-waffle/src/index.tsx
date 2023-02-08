8d4e014df9e3031c664b69c65f8a0731bafdd9ef
littleforest <670177821@qq.com>
2023-02-08 17:27:32 +0800

    fix: 删除入口文件

--- packages/antd-waffle/src/index.tsx
+++ packages/antd-waffle/src/index.tsx
@@ -2,5 +2,4 @@
 export { default as FullScreen } from './full-screen/fullScreen.component';
 export { default as Icons } from './icons/icons.component';
 export { default as Login } from './login/login.component';
-export { default as Video } from './video/video.component';
 export { default as Waterfall } from './waterfall/waterfall.component';
